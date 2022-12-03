faa6f76f5e1b7fb3f94a456737b19cf06a02b03b
Henrique Souza <paulo18santos2000@gmail.com>
2022-12-03 20:36:25 -0300

    Adição das imagens e dos itens "src" pagina in do projeto

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+const route = useRouter()
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo centered-img" src="@/assets/logo.svg" width="125" height="125" />
+
+    <h1 class="centered-text mb-5">Hello World VuejS!</h1>
+
+    <v-tabs
+      bg-color="blue-darken-1"
+      align-tabs="center"
+    >
+      <v-tab value="home" @click="route.push('/')">Home</v-tab>
+      <v-tab value="about" @click="route.push('/about')">Sobre</v-tab>
+      <v-tab value="repos" @click="route.push('/repos')">Repositórios</v-tab>
+      <v-tab value="novo" @click="route.push('/new')">Novo</v-tab>
+    </v-tabs>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+
+<style scoped>
+  .menu{
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+  }
+  .centered-text{
+    text-align: center;
+  }
+
+  .centered-img{
+    margin: auto;
+    display: block;
+  }
+
+  main{
+    margin:50px auto;
+    max-width: 1000px;
+  }
+</style>
