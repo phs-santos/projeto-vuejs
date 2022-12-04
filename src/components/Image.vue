49f1b97d017372ef21f54394fd3bcdd798aa46bf
Henrique Souza <paulo18santos2000@gmail.com>
2022-12-03 22:23:51 -0300

    Melhoria na view New

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,55 +1,57 @@
 <script setup>
-  //Declaração de props
-  const props = defineProps({
-    url: {
-      type: String,
-      required: true
-    },
-    text: {
-      type: String,
-      required: true
-    },
-    hideButton: {
-      type: Boolean,
-      default: false
-    }
-  })
+//Declaração de props
+const props = defineProps({
+	url: {
+		type: String,
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	hideButton: {
+		type: Boolean,
+		default: false
+	}
+})
 
-  //Declaração de Métodos
-  function message() {
-    window.alert("Isso é tudo pessoal")
-  }
+//Declaração de Métodos
+function message() {
+	window.alert("Isso é tudo pessoal")
+}
 </script>
 
 <template>
-  <h1>{{ props.text }}</h1>
+	<h1>{{ props.text }}</h1>
 
-  <div>
-    <img :src="props.url" :alt="props.text ">
-  </div>
+	<div>
+		<img :src="props.url" :alt="props.text">
+	</div>
 
-  <div v-if="(hideButton == false)">
-    <button @click="message()">Clique aqui</button>
-  </div>
+	<div v-if="(hideButton == false)">
+		<v-btn append-icon="mdi-vuetify" @click="message()">
+			Clique aqui
+		</v-btn>
+	</div>
 
-  <v-divider />
+	<v-divider />
 </template>
 
 <style scoped>
-  body{
-    margin-left: 15px;
-  }
-  hr{
-    margin: 20px 0 20px 0;
-  }
-  button{
-    border-radius: 60px;
-    border: 1px solid #1E88E5;
-    padding: 10px 20px;
-    text-transform: uppercase;
-    color: #1E88E5;
-  }
-  h1{
-    color:rgb(82, 81, 81);
-  }
+body {
+	margin-left: 15px;
+}
+
+hr {
+	margin: 20px 0 20px 0;
+}
+
+button {
+	text-transform: uppercase;
+	color: #1E88E5;
+}
+
+h1 {
+	color: rgb(82, 81, 81);
+}
 </style>
--- src/views/New.vue
+++ src/views/New.vue
@@ -1,22 +1,48 @@
+<template>
+
+	<v-carousel class="design">
+		<v-carousel-item
+			src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+			cover
+		></v-carousel-item>
+
+		<v-carousel-item
+			src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+			cover
+		></v-carousel-item>
+
+		<v-carousel-item
+			src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+			cover
+		></v-carousel-item>
+	</v-carousel>
+
+	<div v-for="img in images" :key="img" >
+		<Image :url="img.url" :text="img.text" />
+	</div>
+</template>
+
+
 <script setup>
-import Image from '../components/Image.vue'
-import {ref} from "vue"
+	import Image from '../components/Image.vue'
+	import { ref } from "vue"
 
-const images = ref([
-  {
-    url: 'https://via.placeholder.com/1000x350.png',
-    text: 'Imagem de exemplo 1'
-  },
-  {
-    url: 'https://via.placeholder.com/1000x350.png',
-    text: 'Imagem de exemplo 2'
-  }
-])
-
+	const images = ref([
+		{
+			url: 'https://via.placeholder.com/1000x350.png',
+			text: 'Imagem de exemplo 1'
+		},
+		{
+			url: 'https://via.placeholder.com/1000x350.png',
+			text: 'Imagem de exemplo 2'
+		}
+	])
 </script>
 
-<template>
-  <div  v-for="img in images" :key="img">
-    <Image :url="img.url"  :text="img.text" />
-  </div>
-</template>
+<style>
+
+	.design{
+		margin-bottom: 5rem;
+	}
+
+</style>
